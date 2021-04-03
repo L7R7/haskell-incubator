2c7d62c3165d3add3e2093ebeb58d1527b936295
Leonhard Riedisser <leo008180@googlemail.com>
2021-04-03 22:32:22 +0200

    Add minimal logging example with co-log

--- logging/src/Lib.hs
+++ logging/src/Lib.hs
@@ -1,6 +1,33 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+
 module Lib
-    ( someFunc
-    ) where
+  ( someFunc,
+  )
+where
+
+import Polysemy
+import Polysemy.Log (Log)
+import qualified Polysemy.Log as Log
+import Polysemy.Log.Colog
+import Polysemy.Resource
+import Polysemy.Time (interpretTimeGhc)
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc =
+  runFinal
+    . embedToFinal @IO
+    . interpretTimeGhc
+    . runResource
+    . interpretCologConcNative
+    . interpretLogColog
+    $ prog
+
+prog :: Member Log r => Sem r ()
+prog = do
+  Log.debug "debugging"
+  Log.info "info"
+  Log.error "failing"
